09698785f3f842fa9a122fc6d94e0bd69ba26d66
ImHyeLim1209 <ihl279090@gmail.com>
2021-05-12 17:48:27 +0900

    apply recoil count, setCount

--- recoil-practice/src/Counter.js
+++ recoil-practice/src/Counter.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import { atom, useRecoilState, useResetRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import './App.css';
 
+const countState = atom({
+  key: 'countState',
+  default: 0
+});
+
+
 function App() {
+  //const [count, setCount] = useRecoilState(countState);
+  const count = useRecoilValue(countState);
+  const setCount = useSetRecoilState(countState);
+  const resetCount = useResetRecoilState(countState);
+
+  const increaseCount = () => {
+    setCount((preCount) => preCount + 1);
+  };
+  const decreaseCount = () => {
+    setCount((preCount) => preCount - 1);
+  };
+
   return (
     <div className="App">
       <h1>Counter</h1>
-      <span id="value">count</span>
+      <span id="value">{count}</span>
       <div class="button-container">
-        <button class="btn-decrease">decrease</button>
-        <button class="btn-increase">increase</button>
-        <button class="btn-reset">reset</button>
+        <button class="btn-increase" onClick={increaseCount}>increase</button>
+        <button class="btn-decrease" onClick={decreaseCount}>decrease</button>
+        <button class="btn-reset" onClick={resetCount}>reset</button>
       </div>
     </div>
   );
 }
 export default App;
+
+
+
+
